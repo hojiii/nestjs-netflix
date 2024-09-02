97036bd291111437d37942547215468c9f2536c0
hojji <shimn3060@naver.com>
2024-09-03 01:06:29 +0900

    feat: nestjs 기본 컨트롤러 로직 작성

---
+++ src/app.controller.ts
@@ -0,0 +1,83 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+
+interface Movie {
+  id: number;
+  title: string;
+}
+
+@Controller('movie') //공통적인api를 묶어줌 ()에 값을 넣으면 하위메서드의 기본값이 생성
+export class AppController {
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
+  private idCounter = 3;
+  constructor(private readonly appService: AppService) {}
+  @Get()
+  getMovies(@Query('title') title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+    return this.movies.filter((m) => m.title.startsWith(title));
+  }
+
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID값의 입력입니다.');
+    }
+    return movie;
+  }
+
+  @Post()
+  //가져올값을 body에 넣어줘야함
+  postMovie(@Body('title') title: string) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title,
+    };
+    this.movies.push(movie);
+    return movie;
+  }
+
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body('title') title: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID값의 입력입니다.');
+    }
+    Object.assign(movie, { title }); //덮어씌우기
+    return movie;
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    const movieIndex = this.movies.findIndex((m) => m.id === +id);
+    if (movieIndex === -1) {
+      throw new NotFoundException('존재하지 않는 ID값의 입력입니다.');
+    }
+    this.movies.splice(movieIndex, 1);
+
+    return id;
+  }
+}
