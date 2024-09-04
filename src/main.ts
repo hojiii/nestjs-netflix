b63d40c9ef91b0a4348bfe83b2082a32b2735d73
hojji <shimn3060@naver.com>
2024-09-04 21:29:13 +0900

    feat: validator 문자, 숫자, 커스텀, main.ts에서 pipe 옵션

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //선언한 Key 이외의 값을 넣지 못하게
+      whitelist: true,
+      //선언한 key 이외의 값이 들어가면 에러
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,24 +1,70 @@
 import {
+  Contains,
   Equals,
+  IsAlphanumeric,
   IsArray,
   IsBoolean,
+  IsCreditCard,
   IsDateString,
   IsDefined,
   IsEmpty,
   IsEnum,
+  IsHexColor,
   IsIn,
   IsInt,
+  IsLatLong,
+  IsNegative,
   IsNotEmpty,
   IsNotIn,
   IsNumber,
   IsOptional,
+  IsPositive,
+  isPositive,
   IsString,
+  IsUUID,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+  NotContains,
   NotEquals,
+  registerDecorator,
+  Validate,
+  ValidationArguments,
+  ValidationOptions,
+  Validator,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
 } from 'class-validator';
 
 enum MovieGenre {
   Fantasy = 'fantasy',
   Action = 'action',
+}
+// @ValidatorConstraint({async: true}) 동기로도 사용가능
+@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+      //비밀번호 길이는 4-8
+      return value.length > 4 && value.length < 8;
+  }
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return `비밀번호의 길이는 5-8자 여야합니다. 입력된 비밀번호: ($value)`;
+  }
+}
+
+function IsPasswordValid(validationOption?: ValidationOptions){
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOption,
+      validator: PasswordValidator,
+    });
+  }
 }
 
 export class UpdateMovieDto {
@@ -57,6 +103,39 @@ export class UpdateMovieDto {
   // @IsArray()
   //enum에 포함된 값만 성공
   // @IsEnum(MovieGenre)
-  @IsDateString()
+  //YYYY-MM-DDTHH:MM:SS.SSSz(utctime)
+  // @IsDateString()
+  //가로 안의 수로 나눠지는값
+  // @IsdDivisibleBy(5)
+  //양수인가
+  // @IsPositive()
+  //음수인가
+  // @IsNegative()
+  //최대값
+  // @Max(100)
+  //최솟값
+  // @Min(0)
+  //가로안의 글자가 들어있어야함
+  // @Contains('hojji')
+  //가로안의 글자가 들어가지않아야함
+  // @NotContains
+  //숫자와 알파벳으로만 이루어져있어야함
+  // @IsAlphanumeric()
+  //xxxx-xxxx-xxxx-xxxx 실제 존재할수있는 카드번호가 들어왔냐 정도만 체크
+  // @IsCreditCard()
+  //6글자로 사용된 색상
+  // @IsHexColor()
+  //16개보다 적게
+  // @MaxLength(16)
+  //4보다 많게
+  // @MinLength(4)
+  //UUID 형태의 맞게
+  // @IsUUID()
+  //위도 경도에 맞게
+  // @IsLatLong()
+  // @Validate(PasswordValidator,{
+  //   message: '다른에러 메세지',
+  // })
+  @IsPasswordValid()
   test: string;
 }
